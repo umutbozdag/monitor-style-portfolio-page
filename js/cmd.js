d1c958401d38199b0efaba41e20fceac5ec88b60
Umut <bozdag321@hotmail.com>
2019-11-25 20:25:03 +0300

    added new commands for cmd

--- js/cmd.js
+++ js/cmd.js
@@ -1,6 +1,18 @@
 var cmdButton = document.getElementById("cmd-button");
 var cmdDiv = document.getElementById("cmd");
 var windowCloseBtn = document.querySelectorAll(".window-close-btn");
+var readOnlyTextArea = document.getElementById("text-area-1");
+var textArea = document.getElementById("text-area-2");
+var consoleResult = document.getElementById("console-result");
+
+readOnlyTextArea.value = `
+date -> Actual date and time 
+cls -> Clear console
+color white -> Make background white and text black
+color black -> Make background black and text white
+color default -> Default
+clear -> Clear window
+`
 
 cmdButton.addEventListener("dblclick", handleClick);
 windowCloseBtn[2].addEventListener("click", closeWindow);
@@ -22,3 +34,28 @@ function handleClick() {
 function closeWindow() {
     cmdDiv.style.visibility = "hidden";
 }
+
+function keyPressed(e) {
+    var key = e.keyCode || e.which;
+    if (key == 13 && textArea.value == "date") {
+        date();
+    }
+
+}
+
+function date() {
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + ' ' + time;
+
+    textArea.value = dateTime;
+    textArea.readOnly = true;
+}
+
+
+
+
+
+
+
